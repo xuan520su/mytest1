861a030d305ca3dc20d40496731371f16b5e3f54
xuan520su <15210857263@sina.cn>
2020-07-04 19:34:03 -0700

    finished the new footer [issue 53]

--- hello.java
+++ hello.java
@@ -1,7 +1,8 @@
 class hello{
 	public static void main(String[] args){
 		int i =0;
-		int j =0;
+		int j =50;
 		System.out.println("deal issue 53");
+		System.out.println("deal issue 53-2");
 }
 }
